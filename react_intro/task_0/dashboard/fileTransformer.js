38e4a80ad8cac174df4936718e81cff854d6c759
namigismyl <namiq.ismayilli15@gmail.com>
2026-02-15 02:56:38 +0400

    fix

--- react_intro/task_0/dashboard/fileTransformer.js
+++ react_intro/task_0/dashboard/fileTransformer.js
@@ -2,7 +2,7 @@
     import path from 'path';
 
     export default {
-        process(sourceText, sourcePath) {
+        process(sourceText, sourcePath,options) {
             return {
                 code: `module.exports = ${JSON.stringify(path.basename(sourcePath))};`,
             };
